0c74a1d3fbcfc49ab904ba2963404888eb2e2ca4
Victor <oyandovic@gmail.com>
2022-06-09 15:45:17 +0300

    feature(ImagePickerBuilder) adds method chainging

--- image-picker/src/main/java/com/nlstechsolutions/libraries/image_picker/ImagePicker.kt
+++ image-picker/src/main/java/com/nlstechsolutions/libraries/image_picker/ImagePicker.kt
@@ -16,7 +16,6 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.fragment.app.FragmentActivity
-import androidx.viewbinding.BuildConfig
 import com.canhub.cropper.CropImageContract
 import com.canhub.cropper.CropImageView
 import com.canhub.cropper.options
@@ -34,10 +33,13 @@ import java.util.*
  * @email oyandovic@gmail.com
  * Created 6/7/22 at 9:06 AM
  */
-class ImagePicker(
-    private val mType: CropType,
-    private val callback: (uri: Uri, image: File) -> Unit
+class ImagePicker internal constructor(
+    builder: Builder
 ) : BottomSheetDialogFragment() {
+
+
+    val mType: CropType = builder.type
+    val callback: ((uri: Uri, image: File) -> Unit)? = builder.callback
 
     private var _binding: ImagePickerDialogBinding? = null
     private val binding get() = _binding!!
@@ -46,6 +48,25 @@ class ImagePicker(
 
     internal enum class PickFrom {
         CAMERA, GALLERY
+    }
+
+    class Builder constructor(private val activity: FragmentActivity) {
+        internal var type: CropType = CropType.FREE
+        internal var callback: ((uri: Uri, image: File) -> Unit)? = null
+
+
+        fun cropType(type: CropType) = apply {
+            this.type = type
+        }
+
+        fun resultUri(callback: (uri: Uri, image: File) -> Unit) = apply {
+            this.callback = callback
+        }
+
+        fun show() = apply {
+            ImagePicker(this).show(activity)
+        }
+
     }
 
     enum class CropType {
@@ -89,7 +110,7 @@ class ImagePicker(
                     takePictureFromCamera()
             }
             PickFrom.GALLERY -> {
-                    takePictureFromGallery()
+                takePictureFromGallery()
             }
         }
 
@@ -117,7 +138,7 @@ class ImagePicker(
                 }
 
                 this.dismiss()
-                callback.invoke(uriContent, uriFile)
+                callback?.invoke(uriContent, uriFile)
 
             }
         }
