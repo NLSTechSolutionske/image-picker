121257f1b8587be299675951842058beb1bdee5c
MamboBryan <mambobryan@gmail.com>
2022-06-20 11:28:11 +0300

    chore(file-path): fixes file not existing in samsung phones

--- app/src/main/java/com/nlstechsolutions/imagepicker/MainActivity.kt
+++ app/src/main/java/com/nlstechsolutions/imagepicker/MainActivity.kt
@@ -2,18 +2,22 @@ package com.nlstechsolutions.imagepicker
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.MutableLiveData
 import com.nlstechsolutions.imagepicker.databinding.ActivityMainBinding
 import com.nlstechsolutions.libraries.image_picker.ImagePicker
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     private val uri = MutableLiveData<Uri?>(null)
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +36,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun pickImage() {
-        ImagePicker.Builder(this).cropType(ImagePicker.CropType.FREE).resultUri { uri, _ ->
+        ImagePicker.Builder(this).cropType(ImagePicker.CropType.FREE).resultUri { uri, file ->
+
+            Log.i(TAG, "pickImage: FILE EXISTS -> ${file.exists()}")
+            Log.i(TAG, "pickImage: FILE URI EXISTS -> ${File(uri.path).exists()}")
+
             this.uri.value = uri
         }.show()
 
